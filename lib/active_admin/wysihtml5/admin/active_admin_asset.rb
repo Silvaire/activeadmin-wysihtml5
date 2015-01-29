d43813a5a394e5c07f07bcd4ca183711c9c1acf4
Silvain <silvain@agencyleroy.com>
2015-01-29 10:59:49 +0200

    final fix for class name change

--- lib/active_admin/wysihtml5/admin/active_admin_asset.rb
+++ lib/active_admin/wysihtml5/admin/active_admin_asset.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register ActiveAdminAsset do
 
-  index as: :grid do |asset|
-    link_to(image_tag(asset.storage.thumb("100x100#").url), admin_asset_path(asset))
+  index as: :grid do |active_admin_asset|
+    link_to(image_tag(active_admin_asset.storage.thumb("100x100#").url), admin_active_admin_asset_path(active_admin_asset))
   end
 
   form do |f|
@@ -14,44 +14,44 @@ ActiveAdmin.register ActiveAdminAsset do
   show do
     attributes_table do
       row('Dimensions') do
-        "#{asset.storage.width}px x #{asset.storage.height}px"
+        "#{active_admin_asset.storage.width}px x #{active_admin_asset.storage.height}px"
       end
       row('Thumbnail') do
-        image_tag(asset.thumb_url)
+        image_tag(active_admin_asset.thumb_url)
       end
       row('25%') do
-        image_tag(asset.percentage_thumb_url(0.25))
+        image_tag(active_admin_asset.percentage_thumb_url(0.25))
       end
       row('50%') do
-        image_tag(asset.percentage_thumb_url(0.5))
+        image_tag(active_admin_asset.percentage_thumb_url(0.5))
       end
       row('75%') do
-        image_tag(asset.percentage_thumb_url(0.75))
+        image_tag(active_admin_asset.percentage_thumb_url(0.75))
       end
       row('Full Image') do
-        image_tag(asset.storage.url)
+        image_tag(active_admin_asset.storage.url)
       end
     end
   end
 
   controller do
     def permitted_params
-      params.permit asset: [:storage, :retained_storage, :remove_storage, :storage_url]
+      params.permit active_admin_asset: [:storage, :retained_storage, :remove_storage, :storage_url]
     end
     def create
       # If an app is using Rack::RawUpload, it can just use
       # params['file'] and not worry with original_filename parsing.
       if params['file']
-        @asset = Asset.new
-        @asset.storage = params['file']
+        @active_admin_asset = ActiveAdminAsset.new
+        @active_admin_asset.storage = params['file']
 
-        if @asset.save!
+        if @active_admin_asset.save!
           render json: { success: true }.to_json
         else
           render nothing: true, status: 500 and return
         end
       elsif params['qqfile']
-        @asset = Asset.new
+        @active_admin_asset = ActiveAdminAsset.new
         io = request.env['rack.input']
         # throw io
 
@@ -60,8 +60,8 @@ ActiveAdmin.register ActiveAdminAsset do
 
         # io.original_filename = params['qqfile']
 
-        @asset.storage = Dragonfly::TempObject.new(io.respond_to?(:string) ? io.string : io.read)
-        if @asset.save!
+        @active_admin_asset.storage = Dragonfly::TempObject.new(io.respond_to?(:string) ? io.string : io.read)
+        if @active_admin_asset.save!
           render json: { success: true }.to_json
         else
           render nothing: true, status: 500 and return
